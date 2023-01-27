d5910dd88c663d91fcc7e2b3c78d3d40a4b8fe8c
mauriani <maurianim@gmail.com>
2023-01-27 14:43:05 -0300

    tutorial

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import firestore from "@react-native-firebase/firestore";
 
-import App from './App';
+if (__DEV__) {
+  // se tiver um emulador ele mata ele e inicia outro
+  firestore()
+    .terminate()
+    .then(() => {
+      firestore()
+        .clearPersistence()
+        .then(() => {
+          firestore().useEmulator("localhost", 8080);
+        })
+        .catch(() => {
+          console.log("Clear persistence error");
+        });
+    })
+    .catch((e) => {
+      console.log("Terminae error");
+    });
+}
+
+firestore();
+
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import storage from "@react-native-firebase/storage";
 
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
 
-import { Container, Content, Progress, Transferred } from './styles';
+import { Container, Content, Progress, Transferred } from "./styles";
 
 export function Upload() {
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
+  const [bytesTransferred, setBytesTransferred] = useState("");
+  const [progress, setProgress] = useState("0");
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if (status == 'granted') {
+    if (status == "granted") {
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         aspect: [4, 4],
@@ -24,7 +28,39 @@ export function Upload() {
         setImage(result.uri);
       }
     }
-  };
+  }
+
+  async function handleUpload() {
+    // pega a extensao da imagem
+    const MINE = image.match(/\.(?:.(?!\.))+$/);
+
+    console.log(MINE);
+    // nome do arquivo
+    const fileName = new Date().getTime();
+    // local onde vc deseja salvar
+    const reference = storage().ref(`/images/${fileName}${MINE}`);
+
+    // envia pegando a url da imagem
+    const uploadTask = reference.putFile(image);
+
+    // usamos o on para acompanhar o status de mudança
+    uploadTask.on("state_changed", (taskSnapshot) => {
+      const percent = (
+        (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *
+        100
+      ).toFixed(0);
+
+      setProgress(percent);
+      setBytesTransferred(
+        `${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`
+      );
+    });
+
+    // qunado finalizar da um alert
+    uploadTask.then(() => {
+      Alert.alert("Upload concluído com sucesso");
+    });
+  }
 
   return (
     <Container>
@@ -33,18 +69,11 @@ export function Upload() {
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
-        <Button
-          title="Fazer upload"
-          onPress={() => { }}
-        />
+        <Button title="Fazer upload" onPress={handleUpload} />
 
-        <Progress>
-          0%
-        </Progress>
+        <Progress>{progress}%</Progress>
 
-        <Transferred>
-          0 de 100 bytes transferido
-        </Transferred>
+        <Transferred>{bytesTransferred}</Transferred>
       </Content>
     </Container>
   );
